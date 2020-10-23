4edb3274cc9123fe13becdd22c5244164cead1fb
ubw emiya <a6m3.zero.f@gmail.com>
2020-10-23 13:34:59 +0900

    yy

--- TankFlanker/DxLib_VC/DX_VC/DxCompileConfig.h
+++ TankFlanker/DxLib_VC/DX_VC/DxCompileConfig.h
@@ -2,7 +2,7 @@
 //
 //		ＤＸライブラリ　コンパイルコンフィグヘッダファイル
 //
-//				Ver 3.22 
+//				Ver 3.21e
 //
 // ----------------------------------------------------------------------------
 
@@ -280,9 +280,6 @@
 	#endif
 	#ifndef DX_NOTUSE_DRAWFUNCTION
 		#define DX_NOTUSE_DRAWFUNCTION
-	#endif
-	#ifndef DX_NON_LIVE2D_CUBISM4
-		#define DX_NON_LIVE2D_CUBISM4
 	#endif
 #endif // DX_NON_GRAPHICS
 
